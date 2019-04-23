ac20f1faef636ab72f8f33b29e4e0bb6b1a29676
Porter Savage <theportersavage@gmail.com>
2019-04-23 10:34:20 -0700

    Looping and styling practice

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,12 +1,35 @@
 import React from "react";
 import Ticket from "./Ticket";
 
+var masterTicketList = [
+  {
+    names: 'Thato and Hailey',
+    location: '3A',
+    issue: 'Firebase won\'t save record. Halp.'
+  },
+  {
+    names: 'Sleater and Kinney',
+    location: '4B',
+    issue: 'Fox image not displaying on page, can only see duck?'
+  },
+  {
+    names: 'Imany & Jacob',
+    location: '9f',
+    issue: 'Donkey picture not displaying on hober in Zoology app. :('
+  }
+];
+
 function TicketList(){
   return (
-    <Ticket
-      location="3A"
-      names="Thato and Haley"
-      issue="Firebase will not save record!"/>
+    <div>
+      <hr/>
+      {masterTicketList.map((ticket, index) =>
+        <Ticket names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue}
+          key={index}/>
+      )}
+    </div>
   );
 }
 
