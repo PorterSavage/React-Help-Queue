6823cc25afd911089a574ae4b6121302f9603b33
Porter Savage <theportersavage@gmail.com>
2019-04-29 13:48:06 -0700

    Added state and user inputed form data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,37 @@
 import React from "react";
 import Header from "./Header";
 import TicketList from "./TicketList";
+import NewTicketControl from './NewTicketControl';
 import { Switch, Route } from 'react-router-dom';
-import { NewTicketForm } from './NewTicketForm';
+import Ticket from "./Ticket";
 
+class App extends React.Component {
 
-function App(){
-  return (
-  <div>
-    <Header/>
-    <Switch>
-      <Route exact path='/' component={TicketList}/>
-      <Route path='/newticket' component={NewTicketForm}/>
-    </Switch>
-  </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterTicketList: []
+    };
+    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+  }
+
+  handleAddingNewTicketToList(newTicket){
+    let newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.push(newTicket);
+    this.setState({masterTicketList: newMasterTicketList});
+  }
+
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' render={() =><TicketList ticketList={this.state.masterTicketList}/>}/>
+          <Route path='/newticket' render={() =><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList}/>}/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
